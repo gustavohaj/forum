071fa52ecd099914083a792a3537c9f580be9543
gustavojesus <gustavohenriqueaj@gmail.com>
2021-09-25 21:35:01 -0300

    gitIgnore

---
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,33 @@
+package br.com.alura.forum.controller;
+
+
+import br.com.alura.forum.controller.dto.TopicoDTO;
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class TopicosController  {
+
+    @Autowired
+    private TopicoRepository topicoRepository;
+
+    @RequestMapping("/topicos")
+    public List<TopicoDTO> lista(String nomeCurso){
+        if(nomeCurso == null){
+            List<Topico> topicos = topicoRepository.findAll();
+            return TopicoDTO.converter(topicos);
+        } else {
+            List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+            return TopicoDTO.converter(topicos);
+        }
+
+    }
+
+}
