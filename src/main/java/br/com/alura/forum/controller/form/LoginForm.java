85831ab517227106283a9d0fd76b9b7680167dbc
gustavojesus <gustavohenriqueaj@gmail.com>
2021-09-30 20:54:52 -0300

    Teste de auth

---
+++ src/main/java/br/com/alura/forum/controller/form/LoginForm.java
@@ -0,0 +1,23 @@
+package br.com.alura.forum.controller.form;
+
+public class LoginForm {
+
+    private String senha;
+    private String email;
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
